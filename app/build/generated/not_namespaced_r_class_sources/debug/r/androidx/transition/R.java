d3b8e5298cffa3ce88f266ce04affcd79a7cdeeb
Beatriz <bearm91@gmail.com>
2019-11-17 20:39:23 +0100

    android files

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/transition/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/transition/R.java
@@ -93,29 +93,29 @@ public final class R {
         public static final int icon_group = 0x7f080088;
         public static final int info = 0x7f08008b;
         public static final int italic = 0x7f08008d;
-        public static final int line1 = 0x7f080092;
-        public static final int line3 = 0x7f080093;
-        public static final int normal = 0x7f0800ba;
-        public static final int notification_background = 0x7f0800bb;
-        public static final int notification_main_column = 0x7f0800bc;
-        public static final int notification_main_column_container = 0x7f0800bd;
-        public static final int parent_matrix = 0x7f0800c5;
-        public static final int right_icon = 0x7f0800ce;
-        public static final int right_side = 0x7f0800cf;
-        public static final int save_non_transition_alpha = 0x7f0800d1;
-        public static final int save_overlay_view = 0x7f0800d2;
-        public static final int tag_transition_group = 0x7f080103;
-        public static final int tag_unhandled_key_event_manager = 0x7f080104;
-        public static final int tag_unhandled_key_listeners = 0x7f080105;
-        public static final int text = 0x7f080108;
-        public static final int text2 = 0x7f080109;
-        public static final int time = 0x7f080116;
-        public static final int title = 0x7f080117;
-        public static final int transition_current_scene = 0x7f08011d;
-        public static final int transition_layout_save = 0x7f08011e;
-        public static final int transition_position = 0x7f08011f;
-        public static final int transition_scene_layoutid_cache = 0x7f080120;
-        public static final int transition_transform = 0x7f080121;
+        public static final int line1 = 0x7f080093;
+        public static final int line3 = 0x7f080094;
+        public static final int normal = 0x7f0800bb;
+        public static final int notification_background = 0x7f0800bc;
+        public static final int notification_main_column = 0x7f0800bd;
+        public static final int notification_main_column_container = 0x7f0800be;
+        public static final int parent_matrix = 0x7f0800c6;
+        public static final int right_icon = 0x7f0800d0;
+        public static final int right_side = 0x7f0800d1;
+        public static final int save_non_transition_alpha = 0x7f0800d3;
+        public static final int save_overlay_view = 0x7f0800d4;
+        public static final int tag_transition_group = 0x7f080105;
+        public static final int tag_unhandled_key_event_manager = 0x7f080106;
+        public static final int tag_unhandled_key_listeners = 0x7f080107;
+        public static final int text = 0x7f08010a;
+        public static final int text2 = 0x7f08010b;
+        public static final int time = 0x7f080118;
+        public static final int title = 0x7f080119;
+        public static final int transition_current_scene = 0x7f08011f;
+        public static final int transition_layout_save = 0x7f080120;
+        public static final int transition_position = 0x7f080121;
+        public static final int transition_scene_layoutid_cache = 0x7f080122;
+        public static final int transition_transform = 0x7f080123;
     }
     public static final class integer {
         private integer() {}
@@ -135,7 +135,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0e0056;
+        public static final int status_bar_notification_info_overflow = 0x7f0e0057;
     }
     public static final class style {
         private style() {}
--- app/build/generated/source/buildConfig/debug/com/bearm/owlbotdictionary/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/bearm/owlbotdictionary/BuildConfig.java
@@ -5,7 +5,7 @@ package com.bearm.owlbotdictionary;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.bearm.owldictionary";
+  public static final String APPLICATION_ID = "com.bearm.owlfictionary";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
